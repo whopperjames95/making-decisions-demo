dc8b8d3d164672e4749a38938a9827487aba96d1
James Goff <james.goff1030@gmail.com>
2022-04-14 11:34:18 -0700

    finished demo 2

--- arrays.js
+++ arrays.js
@@ -1,4 +1,31 @@
-/*
-    In this file, we'll be continuing our story
-    with Jon being allowed to flee his fight with Jamie.
-*/
+let backpack = []
+
+backpack.push('Hatchet', 'Water')
+backpack.unshift('Mac n cheese')
+backpack.push('Sleeping bag')
+
+let belt = backpack.splice(1, 1)
+
+
+
+backpack.pop()
+backpack.push('Sleeping bag')
+
+
+let backpack2 = backpack.slice(1, 3)
+
+backpack2.push('Blanket', 'Knife')
+
+// for(let i = 0; i < backpack.length; i++){
+//     console.log(backpack[i])
+// }
+
+// for(let i = 0; i <= backpack2.length - 1; i++){
+//     console.log(backpack2[i])
+// }
+
+for(let i = 0; i < 2; i++){
+    console.log(backpack[i])
+    console.log(backpack2[i])
+}
+
